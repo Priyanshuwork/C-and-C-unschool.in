2fb5713451009c45807e3476c895205e35438683
Priyanshuwork <75384549+Priyanshuwork@users.noreply.github.com>
2021-03-19 13:54:20 +0530

    Here I upload my major project on Casino games

--- main.c
+++ main.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
-#include <string.h>
-#include<stdlib.h>
-struct employee
-{
-    char name[100];
-    char id[20];
-    int experience;
-    float salary;
-};
+#include <stdlib.h>
+
 int main()
 {
-    int i=0,n=5;
-   struct employee e1[n];
-   for(i=0;i<5;i++){
-   printf("\nName of the Employee: ");
-   scanf("%s",e1[i].name);
-   printf("\nID is: ");
-   scanf("%s",e1[i].id);
-   printf("\nExperience is: ");
-   scanf("%d",&e1[i].experience);
-   printf("\nSalary is: ");
-   scanf("%f",&e1[i].salary);}
-   printf(">>>>>>>>>>>>>>>>>>>>>>>Details<<<<<<<<<<<<<<<<<<<<<<<<");
-   printf("\nName: ID is: Experience is: Salary is:\n");
-   for(i=0;i<5;i++){
-   printf("%s\t %s\t %d\t %f\n\n",e1[i].name,e1[i].id,e1[i].experience,e1[i].salary);}
-    return 0;
-}
+    int n,amount,f,r,k,amount1=0,amount2=0,m;
+    char name;
+    printf("WHAT IS YOUR NAME?\n");
+    scanf("%s",&name);
+    printf("What is your age?\n");
+    scanf("%d",&n);
+    printf("What amount you are having right now?\n");
+    scanf("%d",&amount);
+    printf("-------------------------------------------------------------------------------\n");
+    printf("----------------------------------RULES----------------------------------------\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("1. Choose any number between 1 to 10 \n");
+    printf("2. If you win you will get 10 times of your money you bet\n");
+    printf("3. If you bet on wrong number you will lose\n");
+    printf("-------------------------------------------------------------------------------\n");
+    while(amount!=0)
+        {
+        printf("How much money you want to bet?\n");
+        scanf("%d",&f);
+        printf("Guess your number: \t");
+        scanf("%d",&r);
+        k=(rand()%(10-1+1))+1;
+        printf("The number came was: %d\n",k);
+
+        if(r==k)
+        {
+            printf("You won the game\n");
+            amount1= amount*10;
+            printf("Your prize is %d\n",amount1);
+        }
+        else{
+            printf("You lost\n");
+            amount2=amount-f;
+            printf("You are left with %d\n",amount2);
+        }
+        printf("Want to do more?\n 1.yes or 2.no \n");
+        scanf("%d",&m);
+        if(m==1)
+            {
+            printf("game will continue\n");
+        }
+            else if(m==2)
+                {
+                printf("Come back soon\n");
+                break; }
+                else
+                    {
+                    printf("Invalid");
+                    break;
+                }
+                }
+        return 0;
+        }
